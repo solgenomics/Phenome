5d64f2e2f2fe6b9032926baaf4c32a2259df03fb
Naama Menda <nm249@cornell.edu>
2010-03-26 17:05:10 +0000

    some more tweaks to NYBG eggplant loading script
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/Phenome/trunk@17812 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- bin/loading_scripts/load_eggplant_acc.pl
+++ bin/loading_scripts/load_eggplant_acc.pl
@@ -9,22 +9,23 @@ use CXGN::DB::InsertDBH;
 use Getopt::Std;
 
 
-our ($opt_H, $opt_D, $opt_v,  $opt_t, $opt_i, $opt_u, $opt_p);
+our ($opt_H, $opt_D, $opt_v,  $opt_t, $opt_i, $opt_u);
 
-getopts('H:D:u:p:tvi:');
+getopts('H:D:u:tvi:');
 
 my $dbhost = $opt_H;
 my $dbname = $opt_D;
 my $infile = $opt_i;
 my $sp_person=$opt_u;
-#my $sp_person_id = $opt_p || "329";
 
 if (!$infile) {
-    print STDERR "\n You must provide an  infile!\n";
+    print STDOUT "\n You must provide an  infile!\n";
+    usage();
 }
 
 if (!$dbhost && !$dbname) { 
-    print  STDERR "Need -D dbname and -H hostname arguments.\n"; 
+    print  STDOUT "Need -D dbname and -H hostname arguments.\n"; 
+    usage();
 }
 
 my $dbh = CXGN::DB::InsertDBH->new( { dbhost=>$dbhost,
@@ -34,82 +35,85 @@ my $dbh = CXGN::DB::InsertDBH->new( { dbhost=>$dbhost,
 
 #my $person=CXGN::People::Person->new_with_name($dbh, $sp_person);
 my $sp_person_id=CXGN::People::Person->get_person_by_username($dbh, $sp_person);
-
+if (!$sp_prson_id) {
+    print STDOUT "User name $sp_person does not exist in database $bdname on host $dbhost!\n\n";
+    usage();
+}
 
 eval {
-   my $infile=open (my $infile, $opt_i) || die "can't open file $infile!\n";   #./epplant.txt
- 
-   #skip the first line
-   <$infile>;
-   
-
-   my $population_name = "Asian eggplant landraces and allied species";
-   my $desc = "Accessions represented in this germplasm collection are eggplant landraces and closely-related species from various regions of Asia (with a few exceptions).  There are four sources of these collections.  The INRA (France), the AVRDC (Taiwan), the USDA ARS-GRIN (US), and NYBG (US).  These germplasm collections were selected for studying the biodiversity of Asian eggplants.
+    my $infile=open (my $infile, $opt_i) || die "can't open file $infile!\n";   #./epplant.txt
+    
+    #skip the first line
+    <$infile>;
+    
+    
+    my $population_name = "Asian eggplant landraces and allied species";
+    my $desc = "Accessions represented in this germplasm collection are eggplant landraces and closely-related species from various regions of Asia (with a few exceptions).  There are four sources of these collections.  The INRA (France), the AVRDC (Taiwan), the USDA ARS-GRIN (US), and NYBG (US).  These germplasm collections were selected for studying the biodiversity of Asian eggplants.
 Some of the INRA collections are divided by group.  These groups refer to Richard Lester’s classification of eggplants and theory of their domestication.  This classification is not reflected in the other germplasm collections. 
 Several of the accessions have new taxonomic determinations that may differ from the information in the primary germplasm databases (such as the GRIN database).  These new determinations were made by Michael Nee and Rachel Meyer (NYBG).  
 Please contact Rachel Meyer (Rmeyer\@nybg.org) for information about the collection or if you are interested in germplasm requests or donating germplasm.";
-
-
-   my $common_name_id;
-   my $population = CXGN::Phenome::Population->new_with_name($dbh, $population_name);
-   if (!$population) {
-      
-       my $common_name= 'Eggplant';
-       my $org_query= "SELECT common_name_id FROM sgn.common_name WHERE common_name ilike ?";
-       my $org_sth=$dbh->prepare($org_query);
-       $org_sth->execute($common_name);
-       ($common_name_id)=$org_sth->fetchrow_array();
-
-       $population = CXGN::Phenome::Population->new($dbh);
-       $population->set_name($population_name);
-       $population->set_description($desc);
-       $population->set_common_name_id($common_name_id);
-       $population->set_sp_person_id($sp_person_id);
-       $population->store();
-   }
-   
-   while (my $line=<$infile>) {
-       
-       
-       my @fields = split "\t", $line;
-       my $cname= $fields[0];
-       my $accession = $fields[1];
-       my $organism= $fields[2];
-       my $origin = $fields[3];
-       my $comments = $fields[4];
-       my $source= $fields[5];
-       my $photo_acc = $fields[6];
-       chomp $photo_acc;
-       
-       my $description = "";
-       $description .= "Common name: $cname\n" if $cname;
-       $description .= "Organism: $organism\n" if $organism;
-       $description .= "Origin: $origin\n" if $origin;
-       $description .= "Source database: $source\n" if $source;
-       $description .= $comments;
-       
-       my $date= 0;
-       if ($date ==0) {$date= 'now()'};
-       $date=~ s/^(\d{8})(\d{6})/\1 \2-03/;
-       chomp $date;
-       #print STDOUT "$common_name (id = $common_name_id),  $cname, $accession\n";
-       
-       
-       my $ind= CXGN::Phenome::Individual->new($dbh);
-       my @exists= CXGN::Phenome::Individual->new_with_name($dbh, $accession);
-       $ind->set_name($accession);
-       $ind->set_description($description);
-       $ind->set_sp_person_id($sp_person_id);
-       $ind->set_population_id($population->get_population_id());
-       $ind->set_common_name_id($common_name_id);
-       if (!@exists && $description) {
-	   print STDOUT "$accession  $description \n";
-	   $ind->store();
-	   $ind->add_individual_alias($photo_acc, $sp_person_id) if $photo_acc;
-	 	   
-       }else { print STDOUT "individual name exists ($exists[0] , $accession) !\n "; }
-       
-   }
+    
+    
+    my $common_name_id;
+    my $population = CXGN::Phenome::Population->new_with_name($dbh, $population_name);
+    if (!$population) {
+	
+	my $common_name= 'Eggplant';
+	my $org_query= "SELECT common_name_id FROM sgn.common_name WHERE common_name ilike ?";
+	my $org_sth=$dbh->prepare($org_query);
+	$org_sth->execute($common_name);
+	($common_name_id)=$org_sth->fetchrow_array();
+	
+	$population = CXGN::Phenome::Population->new($dbh);
+	$population->set_name($population_name);
+	$population->set_description($desc);
+	$population->set_common_name_id($common_name_id);
+	$population->set_sp_person_id($sp_person_id);
+	$population->store();
+    }
+    
+    while (my $line=<$infile>) {
+	
+	
+	my @fields = split "\t", $line;
+	my $cname= $fields[0];
+	my $accession = $fields[1];
+	my $organism= $fields[2];
+	my $origin = $fields[3];
+	my $comments = $fields[4];
+	my $source= $fields[5];
+	my $photo_acc = $fields[6];
+	chomp $photo_acc;
+	
+	my $description = "";
+	$description .= "Common name: $cname\n" if $cname;
+	$description .= "Organism: $organism\n" if $organism;
+	$description .= "Origin: $origin\n" if $origin;
+	$description .= "Source database: $source\n" if $source;
+	$description .= $comments;
+	
+	my $date= 0;
+	if ($date ==0) {$date= 'now()'};
+	$date=~ s/^(\d{8})(\d{6})/\1 \2-03/;
+	chomp $date;
+	#print STDOUT "$common_name (id = $common_name_id),  $cname, $accession\n";
+	
+	
+	my $ind= CXGN::Phenome::Individual->new($dbh);
+	my @exists= CXGN::Phenome::Individual->new_with_name($dbh, $accession);
+	$ind->set_name($accession);
+	$ind->set_description($description);
+	$ind->set_sp_person_id($sp_person_id);
+	$ind->set_population_id($population->get_population_id());
+	$ind->set_common_name_id($common_name_id);
+	if (!@exists && $description) {
+	    print STDOUT "$accession  $description \n";
+	    $ind->store();
+	    $ind->add_individual_alias($photo_acc, $sp_person_id) if $photo_acc;
+	    
+	}else { print STDOUT "individual name exists ($exists[0] , $accession) !\n "; }
+	
+    }
 };   
 
 if($@) {
@@ -128,4 +132,7 @@ if($@) {
 }
 
 
-
+sub usage { 
+    print STDOUT "Usage: load_eggplant_acc.pl -D dbname [ cxgn | sandbox ]  -H dbhost -t [trial mode ] -i input file -u sgn username \n";
+    exit();
+}
