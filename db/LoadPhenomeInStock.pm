adeb2024f9e253650d66463171cd6843227b5858
Naama Menda <nm249@cornell.edu>
2011-03-04 17:19:15 -0500

    silly typo

--- db/LoadPhenomeInStock.pm
+++ db/LoadPhenomeInStock.pm
@@ -321,11 +321,11 @@ sub patch {
 		    # get the project
 		    my $project_name = $population->get_name;
 		    my @pop_owners = $population->get_owners;
-		    my $project_desc = 'Phenotypes recorded for population $project_name';
+		    my $project_desc = "Phenotypes recorded for population $project_name";
 		    if ( defined($pop_owners[0]) ) {
 			print "owner is " . $pop_owners[0] . "\n" ;
 			my $owner = CXGN::People::Person->new($self->dbh, $pop_owners[0]);
-			$project_desc .= 'by ' . $owner->get_first_name . ' ' . $owner->get_last_name ;
+			$project_desc .= ' by ' . $owner->get_first_name . ' ' . $owner->get_last_name ;
 		    }
 		    my $project = $schema->resultset("Project::Project")->find_or_create( {
 			name => $project_name,
