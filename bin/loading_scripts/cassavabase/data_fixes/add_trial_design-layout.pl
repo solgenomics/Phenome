9a7ffb94f971956c9d18bb2d6809d851caafd6e3
aco46 <aco46@cornell.edu>
2017-01-17 22:39:39 +0000

    add_trial_design-layout.pl script optimised to be faster

--- bin/loading_scripts/cassavabase/data_fixes/add_trial_design-layout.pl
+++ bin/loading_scripts/cassavabase/data_fixes/add_trial_design-layout.pl
@@ -2,11 +2,12 @@
 
 =head1 NAME
 
-add_trial_design-layout.pl - add design and layout to trials without design.
-                           - It uses the TrialDesignStore function.
-                           - implemented for earlier trials uploaded from backend.
-													 - get the plot and block number from the stock names (plot names).
-											 		 - if plot and block number is not found in the plot name, it generate sequence of numbers as plot number, use 1 as the block number and set design to 'CRD'.
+add_trial_design-layout.pl
+ - add design and layout to trials without design.
+ - It uses the TrialDesignStore function.
+ - implemented for earlier trials uploaded from backend.
+ - get the plot and block number from the stock names (plot names).
+ - if plot and block number is not found in the plot name, it generate sequence of numbers as plot number, use 1 as the block number and set design to 'CRD'.
 
 
 =head1 DESCRIPTION
@@ -19,7 +20,7 @@ Options:
  -D the database name
 
  trial_file.txt : A file with two columns: trial name, trial design.
- 									- also works if the trial design column is empty.
+ 		- also works if the trial design column is empty.
 
 =head1 AUTHOR
 
@@ -64,142 +65,120 @@ my $dbh = CXGN::DB::InsertDBH->new( { dbhost=>"$dbhost",
 				 } );
 
 my $schema = Bio::Chado::Schema->connect( sub { $dbh->get_actual_dbh() });
+my $program_object = CXGN::BreedersToolbox::Projects->new( { schema => $schema });
+
+#get design cvterm_id
+my $design_cvterm_id = $schema->resultset("Cv::Cvterm")->search( {name => 'design' }, )->first->cvterm_id;
+my $accession_cvterm_id = $schema->resultset("Cv::Cvterm")->search( {name => 'accession' }, )->first->cvterm_id;
 
 #opening input file and retrieving trial_names and trial_designs
 open (my $file_fh, "<", $file ) || die ("\nERROR: the file $file could not be found\n" );
 
 my $header = <$file_fh>;
 while (my $line = <$file_fh>) {
-		chomp $line;
+  chomp $line;
+  my ($trial_name,$trial_design) = split("\t", $line);
 
-		my ($trial_name,$trial_design) = split("\t", $line);
-	 push @trial_names, $trial_name;
-	 push @trial_designs, $trial_design;
-}
-
-my $program_object = CXGN::BreedersToolbox::Projects->new( { schema => $schema });
-
-for (my $n=0; $n<scalar(@trial_names); $n++) {    #looping through the trial names
-#foreach my $name (@trial_names){
-	my $name = $trial_names[$n];
   my %design_entry;
-  my $design_type = $trial_designs[$n];
   my @plot_numbers_array;
   my @block_numbers_array;
   my @plot_names_array;
   my @stock_id_array;
   my @accession_array;
-	my $project_id;
+  my $project_id;
 
-	if (!$design_type){
-		$design_type = 'RCBD';
+	if (!$trial_design){
+		$trial_design = 'RCBD';
 	}
-  print "Adding design and layout for trial: $name\n";
+  print "Adding design and layout for trial: $trial_name\n";
 
-	my $coderef = sub {
+	#getting trial id from trial name provided
+	$project_id = $schema->resultset("Project::Project")->search({name => $trial_name})->first->project_id;
+  print "PROJECT_ID: $project_id\n";
 
-		#getting trial id from trial name provided
-		$project_id = $schema->resultset("Project::Project")->search({name => $name})->first->project_id;
-    print "PROJECT_ID: $project_id\n";
-
-		#get design cvterm_id
-		my $design_cvterm_id = $schema->resultset("Cv::Cvterm")->search( {name => 'design' }, )->first->cvterm_id;
-		#search if trial has design
-		my $find_design = $schema->resultset('Project::Projectprop')->find(
+	#get design cvterm_id
+	my $design_cvterm_id = $schema->resultset("Cv::Cvterm")->search( {name => 'design' }, )->first->cvterm_id;
+	#search if trial has design
+	my $find_design = $schema->resultset('Project::Projectprop')->find(
 			{
 				project_id => $project_id,
 				type_id => $design_cvterm_id
 			});
 
-		#Insert design if trial has no design
-		if (!$find_design){
-			my $set_design = $schema->resultset('Project::Projectprop')->create(
+	#Insert design if trial has no design
+	if (!$find_design){
+		my $set_design = $schema->resultset('Project::Projectprop')->create(
 				{
 					project_id => $project_id,
 					type_id => $design_cvterm_id,
-					value => $design_type
+					value => $trial_design
 				});
-		}else {
-			print "Design $find_design found for this trial...\n";
+	}else {
+		print "Design $find_design found for this trial...\n";
+	}
+
+	# get trial location name and id
+	my $trials = CXGN::Trial->new({bcs_schema=>$schema, trial_id=>$project_id});
+	my $location = $trials->get_location();
+	#print STDERR Dumper($location);
+	my $trial_location = @{$location}[1];
+	$trial_location_id = @{$location}[0];
+	print "LOCATION FOR THIS TRIAL: $trial_location\n";
+
+	# get stock names and ids (plot names) for a given trial using the trial id
+	# from the stock names get the plot and block number
+	# if plot and block number is not found in the plot name, generate sequence of number as plot number, use 1 as the block number and set design to CRD
+  $plot_number = 1;
+	my $q_2 = "select stock_id,uniquename from stock join nd_experiment_stock using(stock_id) join nd_experiment_project using (nd_experiment_id) join project using(project_id) where project.project_id=?";
+	my $h_2 = $dbh->prepare($q_2);
+	$h_2->execute($project_id);
+
+	while (my ($stock_id, $stock_name) = $h_2->fetchrow_array()) {
+
+		if (my ($plot) = ($stock_name =~ m/plot:([\d]+)_/) || ($stock_name =~ m/plot_([\d]+)_/) ) {
+			print STDERR "Matched plot number $plot in plot $stock_name\n";
+			$plot_number = $plot;
 		}
-
-		# get trial location name and id
-		my $trials = CXGN::Trial->new({bcs_schema=>$schema, trial_id=>$project_id});
-		my $location = $trials->get_location();
-		#print STDERR Dumper($location);
-		my $trial_location = @{$location}[1];
-		$trial_location_id = @{$location}[0];
-		print "LOCATION FOR THIS TRIAL: $trial_location\n";
-
-		# get stock names and ids (plot names) for a given trial using the trial id
-		# from the stock names get the plot and block number
-		# if plot and block number is not found in the plot name, generate sequence of number as plot number, use 1 as the block number and set design to CRD
-    $plot_number = 1;
-		my $q_2 = "select stock_id,uniquename from stock join nd_experiment_stock using(stock_id) join nd_experiment_project using (nd_experiment_id) join project using(project_id) where project.project_id=?";
-		my $h_2 = $dbh->prepare($q_2);
-		$h_2->execute($project_id);
-
-		while (my ($stock_id, $stock_name) = $h_2->fetchrow_array()) {
-
-					if (my ($plot) = $stock_name =~ m/plot:([\d]+)_/) {
-						print STDERR "Matched plot number $plot in plot $stock_name\n";
-						$plot_number = $plot;
-					}
-          elsif (my ($plot_1) = $stock_name =~ m/plot_([\d]+)_/) {
-            print STDERR "Matched plot number $plot_1 in plot $stock_name\n";
-						$plot_number = $plot_1;
-          }else {
-            $plot_number++;
-          }
-
-					if (my ($block) = $stock_name =~ m/replicate:([\d]+)_/) {
-						print STDERR "Matched block number $block in plot $stock_name\n";
-						$block_number = $block;
-					}
-          elsif (my ($block_1) = $stock_name =~ m/_([\d])_/) {
-            print STDERR "Matched block number $block_1 in plot $stock_name\n";
-						$block_number = $block_1;
-          }else {
-            $block_number = 1;
-            $design_type = 'CRD';
-          }
-          push @plot_numbers_array, $plot_number;
-          push @block_numbers_array, $block_number;
-          push @plot_names_array, $stock_name;
-          push @stock_id_array, $stock_id;
-      }
-
-			# get accession used in each plot for a trial
-			# generate design hash for TrialDesignStore function
-      for (my $n=0; $n<scalar(@stock_id_array); $n++) {
-					my $q_3 = "select uniquename from stock join stock_relationship on stock_id=object_id where subject_id=? and stock.type_id= (select cvterm_id from cv join cvterm using(cv_id) where cvterm.name='accession')";
-					my $h_3 = $dbh->prepare($q_3);
-					$h_3->execute($stock_id_array[$n]);
-
-					while ($accession = $h_3->fetchrow_array()){
-
-								$design_entry{$plot_numbers_array[$n]}->{stock_name} = $accession;
-								$design_entry{$plot_numbers_array[$n]}->{plot_name} = $plot_names_array[$n];
-								$design_entry{$plot_numbers_array[$n]}->{is_a_control} = 0;
-								$design_entry{$plot_numbers_array[$n]}->{block_number} = $block_numbers_array[$n];
-								$design_entry{$plot_numbers_array[$n]}->{plot_number} = $plot_numbers_array[$n];
-      }
+		else {
+      $plot_number++;
     }
-	};
 
-	try {
-			$schema->txn_do($coderef);
+		if (my ($block) = ($stock_name =~ m/replicate:([\d]+)_/) || ($stock_name =~ m/_([\d])_/) ) {
+			print STDERR "Matched block number $block in plot $stock_name\n";
+			$block_number = $block;
+	 	}
+		else {
+      $block_number = 1;
+      $trial_design = 'CRD';
+    }
+    push @plot_numbers_array, $plot_number;
+    push @block_numbers_array, $block_number;
+    push @plot_names_array, $stock_name;
+    push @stock_id_array, $stock_id;
+	}
 
-	} catch {
-			die "Load failed! " . $_ .  "\n" ;
-	};
+	# get accession used in each plot for a trial
+	# generate design hash for TrialDesignStore function
+  for (my $n=0; $n<scalar(@stock_id_array); $n++) {
+		my $q_3 = "select uniquename from stock join stock_relationship on stock_id=object_id where subject_id=? and stock.type_id=?";
+		my $h_3 = $dbh->prepare($q_3);
+		$h_3->execute($stock_id_array[$n],$accession_cvterm_id);
+
+		while ($accession = $h_3->fetchrow_array()){
+			$design_entry{$plot_numbers_array[$n]}->{stock_name} = $accession;
+			$design_entry{$plot_numbers_array[$n]}->{plot_name} = $plot_names_array[$n];
+			$design_entry{$plot_numbers_array[$n]}->{is_a_control} = 0;
+			$design_entry{$plot_numbers_array[$n]}->{block_number} = $block_numbers_array[$n];
+			$design_entry{$plot_numbers_array[$n]}->{plot_number} = $plot_numbers_array[$n];
+    }
+  }
 
 	print STDERR Dumper(\%design_entry);
 	print "PROJECT_ID: $project_id\n";
 	my $design_store = CXGN::Trial::TrialDesignStore->new({
 		bcs_schema => $schema,
 		trial_id => $project_id,
-		design_type => $design_type,
+		design_type => $trial_design,
 		design => \%design_entry,
 		is_genotyping => 0,
 		nd_geolocation_id => $trial_location_id,
@@ -210,11 +189,7 @@ for (my $n=0; $n<scalar(@trial_names); $n++) {    #looping through the trial nam
 	if ($validate_error) {
 		print STDERR "VALIDATE ERROR: $validate_error\n";
 	} else {
-		try {
-		$store_error = $design_store->store();
-	} catch {
-		$store_error = $_;
-		};
+
  }
  if ($store_error) {
 	print STDERR "ERROR SAVING TRIAL!: $store_error\n";
